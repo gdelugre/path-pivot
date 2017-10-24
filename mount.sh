e4040d3fd1d82be39f5799b651a1c02cebb4a422
Guillaume Delugré <bug-reports@subvert.technology>
2017-10-24 22:13:49 +0000

    mount.sh: quotes

--- mount.sh
+++ mount.sh
@@ -6,13 +6,13 @@ if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
 fi
 
 disk="$1"
-root=$(dirname $(realpath "$0"))
-mount_point=${root}/mnt
+root="$(dirname $(realpath "$0"))"
+mount_point="${root}/mnt"
 
 [ -d ${mount_point} ] || mkdir ${mount_point}
 
-${root}/fuse/bin/path-pivot -t 3 -L /tmp/path_pivot.log --pivot-to /etc -n 6 "$disk" "${mount_point}"
-modprobe g_mass_storage file=${root}/mnt/path_pivot.img ro=1
+${root}/fuse/bin/path-pivot -t 3 -L /tmp/path_pivot.log --pivot-to /proc/self -n 3 "$disk" "$mount_point"
+modprobe g_mass_storage file="${root}/mnt/path_pivot.img" ro=1
 
 trap ${root}/umount.sh "SIGINT"
 tail -f /tmp/path_pivot.log
