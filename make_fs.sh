66b88af8f20d07f520b30549a62ae1e8545dd6bd
Guillaume Delugré <bug-reports@subvert.technology>
2017-10-26 19:06:05 +0000

    make_fs: create shortcut directories

--- make_fs.sh
+++ make_fs.sh
@@ -8,6 +8,7 @@ fi
 disk="$1"
 root="$(dirname $(realpath "$0"))"
 mount_point="${root}/mnt"
+dir_levels=39
 
 [ -d ${mount_point} ] || mkdir ${mount_point}
 
@@ -16,19 +17,37 @@ mount "$disk" "$mount_point" || exit 1
 
 cd "$mount_point"
 
-for i in `seq 1 39`; do
+for i in `seq 1 $dir_levels`; do
     echo "Creating directory level $i..."
-    mkdir magic_$i
-    ln -s ./magic_$i/ xxx$i
 
-    for j in `seq 1 256`; do
+    # Each x symbolic link points to a directory prefixed with ./magic_.
+    mkdir magic_$i
+    ln -s ./magic_$i/ x$i
+
+    # Create dummy directory entries.
+    for x in `seq 1 256`; do
         touch entry_$RANDOM
     done
 
-    cd xxx$i
+    cwd=$(pwd)
+    mkdir y$i
+    cd y$i
+
+    # Create a tree: y{i}/x{i+1}/.../x{n} pointing to "./magic_{n}"
+    for j in `seq $[i+1] $[dir_levels-1]`; do
+        mkdir x$j
+        cd x$j
+    done
+    ln -s ./magic_${dir_levels}/ x$dir_levels
+
+    cd "${cwd}/x$i"
+
+    # Create a dummy file entry to fill up space.
     dd if=/dev/urandom of=$RANDOM.bin bs=4M count=1 status=none
 done
 
+# Dummy target files.
 echo "nothing to see here" > maps
+echo "nothing to see here" > passwd
 
 umount "$mount_point"
