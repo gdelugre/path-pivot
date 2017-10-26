378740f309e2ae3787cf53f8a2bba823856ae2ed
Guillaume Delugré <bug-reports@subvert.technology>
2017-10-26 21:37:16 +0000

    make_fs: fallback to previous implementation

--- make_fs.sh
+++ make_fs.sh
@@ -22,25 +22,14 @@ for i in `seq 1 $dir_levels`; do
 
     # Each x symbolic link points to a directory prefixed with ./magic_.
     mkdir magic_$i
-    ln -s ./magic_$i/ x$i
+    ln -s ./magic_$i/ xxx$i
 
     # Create dummy directory entries.
     for x in `seq 1 256`; do
         touch entry_$RANDOM
     done
 
-    cwd=$(pwd)
-    mkdir y$i
-    cd y$i
-
-    # Create a tree: y{i}/x{i+1}/.../x{n} pointing to "./magic_{n}"
-    for j in `seq $[i+1] $[dir_levels-1]`; do
-        mkdir x$j
-        cd x$j
-    done
-    ln -s ./magic_${dir_levels}/ x$dir_levels
-
-    cd "${cwd}/x$i"
+    cd xxx$i
 
     # Create a dummy file entry to fill up space.
     dd if=/dev/urandom of=$RANDOM.bin bs=4M count=1 status=none
